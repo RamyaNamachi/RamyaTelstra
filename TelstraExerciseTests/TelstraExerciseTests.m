18ab449c117c4135d3fbf4b4267998bf322969f0
MSP <Sathiyasuresh@Pepsi-MacMini.local>
2015-03-03 15:28:06 +0530

    Project added

---
+++ TelstraExerciseTests/TelstraExerciseTests.m
@@ -0,0 +1,40 @@
+//
+//  TelstraExerciseTests.m
+//  TelstraExerciseTests
+//
+//  Created by MSP on 03/03/15.
+//  Copyright (c) 2015 Cognizant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TelstraExerciseTests : XCTestCase
+
+@end
+
+@implementation TelstraExerciseTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
